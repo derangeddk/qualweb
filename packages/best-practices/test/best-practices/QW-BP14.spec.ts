966f52590962dc15b58819a5b782204b71574d3c
Martin Vanlichtervelde <martin.vanlichtervelde@deranged.dk>
2026-07-08 11:16:31 +0200

    Modify tests and Merge conflicts

--- packages/best-practices/test/best-practices/QW-BP14.spec.ts
+++ packages/best-practices/test/best-practices/QW-BP14.spec.ts
@@ -1,8 +1,4 @@
 import { resolve } from 'node:path';
 import { buildTest } from './template';
 
-// buildTest('QW-BP14', resolve(__dirname, '../fixtures/testcases/BP14/testcases.json'));
-
-describe('QW-BP14', () => {
-  it('Rule has no implementation');
-});
+buildTest('QW-BP14', resolve(__dirname, '../fixtures/testcases/BP14/testcases.json'));
--- packages/best-practices/test/best-practices/QW-BP3.spec.ts
+++ packages/best-practices/test/best-practices/QW-BP3.spec.ts
@@ -1,8 +1,4 @@
 import { resolve } from 'node:path';
 import { buildTest } from './template';
 
-// buildTest('QW-BP3', resolve(__dirname, '../fixtures/testcases/BP3/testcases.json'));
-
-describe('QW-BP3', () => {
-  it('Rule has no implementation');
-});
+buildTest('QW-BP3', resolve(__dirname, '../fixtures/testcases/BP3/testcases.json'));
